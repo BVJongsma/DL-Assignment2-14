0e0f9c6f21ad297e03e47bcbfd927e955a7d1646
BVJongsma <b.v.jongsma@student.rug.nl>
2022-03-25 16:25:31 +0100

    update??

--- encoder.py
+++ encoder.py
@@ -1,7 +1,31 @@
-import pytorch as pt
+import torch as pt
+from torch.nn import Sequential, Conv2d, MaxPool2d, BatchNorm2d, ReLU
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def encoder(image):
+
+class Net():
+    def __init__(self):
+        super(encoder, self).__init__())
+
+        self.cnn_layers = Sequential(
+        # Defining a 2D convolution layer
+        Conv2d(1, 2, kernel_size=3, padding=1),
+        BatchNorm2d(2),
+        ReLU(inplace=True),
+        MaxPool2d(kernel_size=2, stride=2),
+        # Defining another 2D convolution layer
+        Conv2d(2, 2, kernel_size=3, padding=1),
+        BatchNorm2d(2),
+        ReLU(inplace=True),
+        MaxPool2d(kernel_size=2, stride=2),
+
+    )
+
+    def forward(self, model):
+        model = self.cnn_layers(model)
+        return model
+    """
     model = Sequential()
     model.add(InputLayer(input_shape=(None, None, 1)))
     model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
@@ -16,8 +40,9 @@ def encoder(image):
     model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
     model.add(UpSampling2D((2, 2)))
     model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
+    """
 
-    return output
+
 
 if __name__ == "__main__":
 
