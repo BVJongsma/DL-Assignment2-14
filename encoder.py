1cb558b990b6884eb3a4171ff11cb2ffa5cb388d
BVJongsma <b.v.jongsma@student.rug.nl>
2022-03-25 15:58:55 +0100

    started encoder

---
+++ encoder.py
@@ -0,0 +1,28 @@
+import pytorch as pt
+from sklearn.model_selection import train_test_split
+
+def encoder(image):
+    model = Sequential()
+    model.add(InputLayer(input_shape=(None, None, 1)))
+    model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
+    model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(16, (3, 3), activation='relu', padding='same', strides=2))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
+
+    return output
+
+if __name__ == "__main__":
+
+    #split into train-test
+    data_folder = "../Colorization/L_landscapes" # change this to the tensors later
+    X_train, X_test, y_train, y_test = train_test_split(data_folder + '/L', data_folder + '/a', test_size = 0.33, random_state = 42)
+    epochs = 10
+    latent_space_dimn = 20
